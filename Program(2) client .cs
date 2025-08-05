8cb6800ff4022c63f4e6ff6ab83102ae6d89ddb8
Mai-Trinh123 <trinhntm6631@ut.edu.vn>
2025-08-06 03:04:14 +0700

    Create Program(2) client .cs

---
+++ Program(2) client .cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace RPS_Client
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormClient());
+        }
+    }
+}
